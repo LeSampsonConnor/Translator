a58537cec8d8f0c4d93a7b1a80e45c3b7f076c4c
aashnapshah <34110757+aashnapshah@users.noreply.github.com>
2017-12-19 16:37:17 -0500

    Update piglatin.cpp

--- Translator/piglatin.cpp
+++ Translator/piglatin.cpp
@@ -1,84 +1,52 @@
-#include <iostream>
-#include <string>
-#include<QString>
-#include<QDebug>
-using namespace std;
+#include "piglatin.h"
+#include <QString>
+#include <QChar>
+#include <QStringList>
+#include <QDebug>
+
+
+//Function Decleration
+QString moveLetter(QString);
+bool ifVowel(QChar);
 
 QString PigLatin::getTranslation() {
-
     QString translatedText = "";
 
-    //Declare three functions
-    void pigLatin(Qchar []);
-    Qstring moveLetter(Qstring);
+    QStringList token;
+    token = this->textToTransalted.split(" ");
 
-
-    //Split up string into tokens
-    //Each word is broken up at space
-    Qchar *token;
-    token = strtok(this->textToBeTranslated, " ");
-
-    Qstring PigLatinOutput;
-
-    //Loop through each word for translation
-    //Null pointer is returned at end of string
-    while (token != NULL) {
-        pigLatin(token);
-        token = strtok(NULL," ");
-    }
-    cout << endl;
-
-    return 0;
-
-
-    // Check if first letter is vowel
-    // Implement lowercase function to see if they're equal with vowels in array
-bool PigLatin::ifVowel(Qchar chLetter) {
-        Qchar letters[] = {'a','e','i','o','u','y'};
-        for (int i = 0; i < 6; i++) {
-            if (tolower(chLetter) == letters[i])
-            {
-                return true;
-            }
-        }
-
-        return false;
-
-    }
-
-    // Recursive function to take off a non vowel letter and put it on the end of word
-    // substr allows for specific characters in a string to be asscessed - remove the first letter
-    // Recall the function until a vowel is encountered and then return the word.
-    // If word starts with a vowel, return word
-
-    Qstring moveLetter(Qstring strWord) {
-
-        if (!ifVowel(strWord[0])) {
-            Qchar chLetter = tolower(strWord[0]);
-            return moveLetter(strWord.substr(1) + chLetter);
-        }
-
-        else { return strWord; }
-
-    }
-
-    //Determine if string starts with vowel
-    //If vowel, end with "way"
-    //If not vowel, end with "ay"
-    void pigLatin(Qchar strWord[]) {
-
-        if (ifVowel(strWord[0])) {
-            cout << strWord << "way ";
-        }
-
-        else {
-            cout << moveLetter(strWord) << "ay ";
+    for(int i=0; i<token.length(); i++) {
+        bool boolVowel = ifVowel(token[i][0]);
+        if (boolVowel) {
+            translatedText += token[i] + "way ";
+        } else {
+            translatedText += moveLetter(token[i]) + "ay ";
         }
     }
-
-    for(int i = 0; i<(this->textToBeTranslated.length()); i++) {
-        translatedText += PigLatin[this->textToBeTranslated[i].toUpper()] + " ";
-    }
-
     return translatedText;
 }
+
+bool ifVowel(QChar chLetter) {
+
+    QChar letters[] = {'a','e','i','o','u','y'};
+    for (int i = 0; i < 6; i++) {
+        if (chLetter.toLower() == letters[i])
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+QString moveLetter(QString strWord) {
+
+    if (!ifVowel(strWord[0])) {
+        QChar chLetter = strWord[0].toLower();
+        return moveLetter(strWord.right(strWord.length()-1) + chLetter);
+    } else {
+        return strWord;
+    }
+
+}
+
+
